4b2bee3c7814a4fb1daae4205f99d81106bdca2e
Alexander <alex.m.k20@gmail.com>
2017-04-17 21:47:56 +0300

    react for beginners

---
+++ reactforbeginners/scripts/main.js
@@ -0,0 +1,78 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+
+var App = React.createClass({
+    render : function() {
+        return(
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline="Fresh Seafood Market" />
+                </div>
+                <Order />
+                <Inventory />
+            </div>
+        )
+    }
+});
+
+var Header = React.createClass({
+    render : function() {
+        return(
+            <header className="top">
+                <h1>
+                    Catch
+                    <span className="ofThe">
+                        <span className="of">of</span>
+                        <span className="the">the</span>
+                    </span>
+                    Day
+                </h1>
+                <h3 className="tagline">
+                    <span>{this.props.tagline}</span>
+                </h3>
+            </header>
+        )
+    }
+});
+
+var Order = React.createClass({
+    render : function() {
+        return(
+            <p>
+            </p>
+        )
+    }
+});
+
+var Inventory = React.createClass({
+    render : function() {
+        return(
+            <p>
+            </p>
+        )
+    }
+});
+
+
+var StorePicker = React.createClass({
+    render : function() {
+        return(
+            <form className="store-selector">
+                {/* Simple comment */}
+                <h2>Please Enter A Store</h2>
+                <input type="text" ref="storeId" required />
+                <input type="Submit" />
+            </form>
+        )
+    }
+});
+
+/*
+* Routes
+* */
+
+ReactDOM.render(<App/>, document.querySelector('#main'));
